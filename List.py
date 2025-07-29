c2a75a20fa697bdcc27301f45d08c7d4e460f8b3
anjali-tiwari-AI <tiwari.anjali2020@gmail.com>
2025-07-29 08:54:31 -0400

    List

---
+++ List.py
@@ -0,0 +1,375 @@
+# WAP in python, In list 1 - 100 numbers are stored, one number is missing how do you find it.
+# list = [1, 5, 8, 11, 23, 35, 49, 56, 68, 73, 82, 91]
+# for i in range(1, 101):
+#     if not i in list:
+#         print(i, end = " , ")
+
+# 2. WAP in python for, in a list(array) 1 - 100 multiple numbers are duplicates, how do you find it
+# List = [1, 2 , 3 , 4 , 5, 6 , 7 , 8, 9 , 10 , 11, 12 , 13 , 14 , 15 , 16 , 17 , 18 , 19 , 20 , 21 , 
+#         22 , 23 ,24 , 25 , 26 , 27 , 28 , 29 , 30 , 31 , 32 , 33 , 34 , 35, 36 , 37 , 38 , 39 , 40 , 
+#         41 , 42 , 43 , 44 , 45 , 46 , 47 , 48 , 49, 50 , 51 , 52 , 53 , 54 , 55 , 56, 57 , 58 , 59 , 
+#         60 , 61 , 62 , 63 , 64 , 65 , 66 , 67 , 68, 69 , 70 , 71 , 72 , 73, 74 , 75 , 76 , 77 , 78 , 
+#         79 , 80 , 81 , 82, 83 , 84 , 85 , 86 , 87 , 88 , 89 , 90 , 91, 92 , 93 , 94 , 95 , 96 , 63 ,
+#         97 , 98 , 99 , 100 , 58, 53, 46, 75, 86, 21, 4, 9, 63, 75, 26, 45, 9, 3, 46, 51, 86, 29, 95 , 
+#         75, 46, 42, 19, 28, 43, 85, 42, 42, 22, 16, 19, 11, 22, 33, 44, 88, 66, 77, 88]
+
+# i = 0
+# unique = []
+# Duplicate = []
+# for element in List:
+#     if element not in unique:
+#         unique.append(element)
+#     elif element not in Duplicate:
+#         Duplicate.append(element)
+# print("The unique value:", unique)
+# print("The duplicate value is:", Duplicate)
+
+
+# 3. WAP in python to find first duplicate numbers in a given list(array) -- Done
+# List = [10, 5, 8, 3, 1, 9, 4, 5, 6, 7, 2, 6, 4]
+# count = 1
+# Duplicate = []
+# Unique = []
+# Duplicate_first = []
+# for element in List:
+#         if element not in Unique:
+#                 Unique.append(element)
+#         elif element not in Duplicate:
+#                 Duplicate.append(element)
+#                 if element in Duplicate and Unique:
+#                      Duplicate_first.append(element)
+#                      count = 1
+#                      break   
+# print("Dup:", Duplicate)
+# print("unique:", Unique)
+# print("Duplicate_first:", Duplicate_first)
+
+
+# 4. WAP in python to remove duplicate elements from array(list) - Done
+# List = [1, 2, 3, 4, 5, 6, 7, 8, 9, 2, 5, 7, 9, 3, 4, 6, 1, 8, 8, 6, 6, 7, 3, 4, 9]
+# unique = []
+# duplicate = []
+# for element in List:
+#     if element not in unique:
+#         unique.append(element)
+#     elif element not in duplicate:
+#         duplicate.append(element)
+# print("unique =", unique)
+# print("Duplicate:", duplicate)
+
+
+# 5. WAP in python for, given two arrays 1, 2, 3, 4, 5 and 2, 3, 1, 0, 5 
+# and find which number is not present in the second array. --- Need to check
+# List1 = [1, 2, 3, 4, 5]
+# List2 = [2, 3, 1, 0, 5]
+# Not_present = []
+# NP = []
+# i = 0
+# for element in List1 and List2:
+#     if element in List1 and not List2:
+#         print(element)
+#     elif element in List2 and not List1:
+#         print(element)
+
+
+# 6. WAP in python for, how to compare two array is equal in size or not. -- not able to solve
+# Array1 = [1, 2, 3, 4, 5]
+# Array2 = [1, 2, 4, 1, 4]
+# A  = Array1.sort()
+# B = Array2.sort()
+# if A == B:
+#     print(True)
+# else:
+#     print(False)
+# print(A) 
+# print(B)
+
+
+# for element in Array1 and Array2:
+#     if len(Array1) == len(Array2):
+#         print("Yes, it is equal")
+#         break
+#     else:
+#         print("No, it is not equal")
+#         break
+
+
+# 7.1. WAP in python, to find smallest number in array. - Done
+# A = [] # 5, 9, 2, 4, 6, 3, 7
+# Size = int(input("Enter the size of the list:"))
+# for i in range(Size):
+#     Value = int(input("Enter number:"))
+#     A.append(Value)
+# Minimum = A[0]
+# for i in range(Size):
+#     if A[i]<Minimum:
+#         Minimum = A[i]  
+# print("Minimum value is:", Minimum) 
+
+
+# 7.2. WAP in python, to find largest number in array. - Done
+# Num = [45, 86, 26, 91, 13, 7]
+# Largest = Num[0]
+# for i in range(len(Num)):
+#     if Num[i]>Largest:
+#         Largest = Num[i]
+# print(Largest)
+
+
+# A = []
+# Size = int(input("Enter the size of the number:"))
+# for i in range(Size):
+#     Value = int(input("Enter the number:"))
+#     A.append(Value)
+# Maximum = A[0]
+# for i in range(Size):
+#     if A[i]>Maximum:
+#         Maximum = A[i]
+# print("The all numbers is:", A)
+# print("The maximum number is:", Maximum)        
+
+
+# 8 WAP in python, to find second highest number in an array - Done
+# Size = int(input("Enter the elements(Size of the number:"))
+# Nums = []
+# for i in range(Size):
+#     Num = int(input("Enter the numbers:"))
+#     Nums.append(Num)
+# Largest = Nums[0]
+# Sec_largest = Nums[0]
+# for i in range(len(Nums)):
+#     if Nums[i]>Largest:
+#         Largest = Nums[i]
+# for i in range(len(Nums)):
+#     if Sec_largest<Nums[i] and Nums[i] != Largest:
+#         Sec_largest = Nums[i]
+# print(Sec_largest)
+
+
+# Number = [12, 25, 75, 36, 39, 81, 43, 61, 95, 72, 53]
+# Largest = Number[1]
+# Sec_Largest = Number[0]
+# for i in Number:
+#     if Number>Largest:
+#         Largest = Number
+#         Sec_Largest = Largest
+#         Largest += 1
+#         Sec_Largest += 1
+#         print("Largest:", Largest , "snd second largest:", Sec_Largest)
+#     elif Number>Sec_Largest and Number<Largest:
+#         Sec_Largest = Number
+#         print("Largest:", Largest , "snd second largest:", Sec_Largest)
+#         Largest += 1
+#         Sec_Largest += 1
+
+# A = []
+# Size = int(input("The size of the number is:"))
+# for i in range(Size):
+#     Value = int(input("Enter the number:"))
+#     A.append(Value)
+# Highest = A[0]
+# Sec_high = A[1]
+# High2 = Sec_high[0]
+# for i in range(Size):
+#     if A[i]>Highest:
+#         print("The highest number is:", A[i])
+#         Sec_high.append(A[i])
+#         Highest = A[i]
+#         if Sec_high>High2:
+#             print("The second highest number is:", Sec_high)      
+# print("The highest number is:", Highest)
+# print("The second highest number is:", High2)
+
+
+# 9 WAP in python, to find top two maximum number in an array - Done
+# Array = [42, 75, 34, 98, 23, 10, 46, 37]
+# Max1 = Array[0]
+# Max2 = Array[0]
+# for i in range(len(Array)):
+#     if Array[i]>Max1:
+#         Max1 = Array[i]
+#         print("Max1:", Max1)
+#         for i in range(len(Array)):
+#             if Max1>Array[i] and Array[i]>Max2:
+#                 Max2 = Array[i]
+#                 print("Max2:", Max2)
+# print("The largest first and second maximum number is:", Max1 , "and", Max2)
+
+
+# 10 WAP to print array in reverse order - [Starting:Ending:step(increment or decrement)] - Done
+# List = [45, 36, 76, 96, 82, 23, 56, 61]
+# # print(List[-1::-1])
+# ---- or ----
+# List.reverse()
+# print(List)
+# # ------or----
+# for i in range((len(List)-1), -1, -1):
+#     print(List[i], end = " ")
+
+
+# 11 WAP to reverse an array in two ways - didn't understand the question
+
+
+
+# 12 WAP to calculate length on an array
+# Value = [75, 96, 25, 36, 46]
+# count = 0
+# for i in Value:
+#     count += 1
+# print(count)
+
+
+# 13 WAP to insert an element at the end of an array
+# Array = [42, 85, 36, 61, 52, 29, 75, 94, 16]
+# Array.append(789)
+# print(Array)
+
+
+# 14 WAP to insert element at a given location in array - List.insert(index,value) - Need to check
+# List = [45, 63, 85, 72, 15]
+# Value = 100
+# Position = 2
+# List.append(None)
+# for i in range(len(List)):
+#     List[i+1] = List[i]
+# List[Position-1] = Value
+# print(List)
+
+
+# A = []
+# Size = int(input("Enter the size of the A:"))
+# for i in range (Size):
+#     Value = int(input("Enter the elements:"))
+# Pos = int(input("Enter the position where the element needs to put:"))
+# Key = int(input("Enetr the number which needs to add in A:"))
+# A.append(None)
+# print(A)
+# for i in range(Size-1, Pos-2, -1):
+#     A[i+1]=A[i]
+# A[Pos-1] = Key
+# print(A)
+
+
+
+# ---------or -------
+# List = [75, 25, 43, 16, 67, 82, 92]
+# List.insert(2, 100)
+# print(List)
+
+
+# 15 WAP to delete element at end of array - use pop to delete - Done
+
+# ----or-------
+# List = [56, 75, 25, 36]
+# List.pop(2)
+# print(List)
+
+
+# 16 WAP to delete given element from array - Done
+# A = []
+# Size = int(input('Enter the size of the A:'))
+# Flag = 0
+# for i in range(Size):
+#     Value = int(input("Enter the numbers:"))
+#     A.append(Value)
+# print("Original list is:", A)
+# Key = int(input("Enter the Number which needs to find for deletion:"))
+# for i in range(Size):
+#     if A[i] == Key:
+#         Flag = 1
+#         Pos = i
+#         break
+# if Flag == 0:
+#     print("Element not found")
+# else:
+#     for i in range(Pos, Size-1):
+#         A[i] = A[i+1]
+#         A.pop()
+#         print("Final list after delete the element:", A)
+
+
+# 17 WAP to delete element from array from given index
+List = [12, 34, 56, 78, 90]
+index = 2
+# List.pop(index)
+# print(List)
+#---or----
+# del List[index]
+# print(List)
+
+# ----or -----
+# A = []
+# Size = int(input("Enter the size of A:"))
+# Index = int(input("Enter th index number which needs to delete:"))
+# Flag = 0
+# for i in range(Size):
+#     Value = int(input("Enter the numbers:"))
+#     A.append(Value)
+# for i in range(Size):
+#     if i == Index:
+#         Flag = 1
+#         Position = i
+#         break
+# for i in range(Size):
+#     if Flag == 0:
+#         print("Index not found")
+#     else:
+#         for i in range (Position, Size-1):
+#             A.pop(Index)
+# print("The list after delete the index", A)
+
+
+# 18 WAP to find sum of array elements. - Done
+# List = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+# Sum = 0
+# index = 1
+# for i in List:
+#     Sum = i+Sum
+#     print(Sum)
+# print("The total sum is:", Sum)
+
+
+# 19 WAP to print all even numbers in array
+# List = [45, 25, 26, 76, 23, 94, 81, 46, 10, 28, 37]
+# for i in List:
+#     if i%2 == 0:
+#         print(i, end = " ")
+
+
+# 20 WAP to print all odd numbers in array - Done
+# Odd = [45, 76, 25, 86, 94, 20, 13, 75, 48, 29]
+# for i in Odd:
+#     if i%2 != 0:
+#         print(i , end = " ")
+
+
+# 21 WAP to perform left rotation of array elements by two positions
+A = []
+Size = int(input("Enter the size of the A:"))
+for i in range(Size):
+    Value = int(input("Enter the elements for A:"))
+    A.append(Value)
+print("The original list is:", A)
+Key = A[0]
+Key_2 = A[1]
+for i in range(1, Size-2):
+    A[i-1] = A[i]
+    print(A)
+
+print("After modification the list would be:", A)
+
+
+
+# 22 WAP to perform right rotation in array by 2 positions 
+
+
+# 23 WAP to merge two array - Done
+# Array1 = [12, 32, 45, 36, 55, 66]
+# Array2 = [75, 89, 95, 46, 61, 53]
+# Merge = Array1 + Array2
+# print(Merge)
+
+
+# 24 WAP to find highest frequency element in array
+# 25 WAP to add two number using recursion
+# 26 WAP to find sum of digit of number using recursion.
--- Loops.py
+++ Loops.py
@@ -57,14 +57,19 @@
 #     i += 1
 
 
-#3 WAP to print multiplication table of a given number - done but not able to solve it from while loop
+#3 WAP to print multiplication table of a given number - done p
 # Number = int(input('Enter number:'))
 # for i in range(1, 11):
 #     print(i*Number, end = " ")
 #     i += 1
+#----------or ------------
+# Number = int(input("Enter number:"))
+# Num = 1
+# while Num<=10:
+#     print(Number*Num, end = " ")
+#     Num +=1 
 
-
-#4 WAP to print all natural numbers in reverse order - Done but not able to solve from for loop
+#4 WAP to print all natural numbers in reverse order - Done 
 # Number = int(input("Enter number:"))
 # while Number>=1:
 #     print(Number, end = " ")
@@ -89,12 +94,21 @@
 
 #6 WAP to find sum of even number between 1 to n - Done not able to solve from while
 # N = 20
-# Sum = 0
+# Sum = 1
 # for i in range(1, N+1):
 #     if i%2 == 0:
 #         Sum += i
 # print("The sum of the even number is:", Sum)
-
+# --------------or -------------------
+# Num = int(input("Enter number:"))
+# i = 1
+# Sum = 0
+# while i<=Num:
+#     if i%2 == 0:
+#         print("The sum of the even number is:", i , "and the total sum is:", Sum)
+#     Sum += i
+#     i += 1
+# print("The sum of all the even number is:", Sum)
 
 
 #7 WAP to print sum of odd number between 1 to n - done 
@@ -106,7 +120,7 @@
 # print("Total sum of the odd number is:", Sum) 
 
 
-#8 WAP to swap first and last digit of the number - Done but not able to solve from loop - need to ask 
+#8 WAP to swap first and last digit of the number - Done 
 # Number = 123256938
 # count = 0
 # A = Number%10
@@ -116,7 +130,6 @@
 #     count += 1
 # print("The swap number is:", Number)
 # print(A, Number)
-
 
 
 # Number = 569256
@@ -152,30 +165,78 @@
 # print(Number+A)
 
 
+# 10 WAP to find first and last digit of any number
+# Num = int(input('Enter number:'))
+# Num1 = Num%10
+# i = 1
+# while Num//10 != 0:
+#     Num = Num//10
+#     i += 1
+# print("The first digit is:", Num , "and the last digit is:", Num1)
 
-#10 WAP to find first and last digit of any number
+
+#11 WAP to calculate product of digits of a number - Done
+# Num = int(input("Enter number:"))
+# Digit = 1
+# while Num>0:
+#     Digit = Digit*(Num%10)
+#     print(Digit)
+#     Num = Num//10
+# print("The multiplication of the number is:", Digit)
 
 
 
-#11 WAP to calculate product of digits of a number
-#12 WAP to reverse a number using while and for loop
+#12 WAP to reverse a number using while and for loop - Done
+# Num = int(input('Enter number:'))
+# i = 0
+# while Num>1:
+#     i = Num%10
+#     print(i, end = " ")
+    # Num = Num//10
+# -------for loop----- (Need to ask)
+# Num = int(input("Enter number:"))
+# for i in range(1, len(Num)):
+#     Num = Num%10
+#     print(Num)
+#     i += 1
+
+
 #13 WAP to calculate power using while and for loop
-#14 WAP to find factorial of any number
+# Num = int(input("Enter number:"))
+# Power = int(input("Enter number:"))
+# i = 1
+# R = 1
+# while i<=Power:
+#     R = Num*R
+#     print(R)
+#     i += 1
+# print("The power of the number is:", R)
+
+# #14 WAP to find factorial of any number - Done
+# X = int(input('Enter number:'))
+# i = 1
+# R = 1
+# while i <= X:
+#     R = R*i
+#     print(R)
+#     i += 1
+# print("The factorial of the number", X, "is", R)
+
 #15 WAP to check whether a number is Armstrong number or not
 
 
 
 #16 WAP to find Armstrong numbers between 1 to n
-Num = 586
-length = len(str(Num))
-i = 0
-count = Num[i]
-for i in Num:.
+# Num = 586
+# length = len(str(Num))
+# i = 0
+# count = Num[i]
+# for i in Num:.
 
 
-    Add = count**length
-    print(Add)
-    count += 1
+#     Add = count**length
+#     print(Add)
+#     count += 1
 
 
 #17 WAP to calculate compound interest - Done
@@ -270,3 +331,222 @@ for i in Num:.
 
 
     
+# 1 - Check if All Digits of a Number Are the Same - Done
+# e.g. 1111 → True, 1234 → False
+# Num1 = 8963
+# Num2 = 4563
+# i = 0
+# while i>=Num1:
+#     Num1 = Num1%10
+#     print(Num1)
+#     Num1 = Num1//10
+# if Num1==Num2:
+#     print("Yes, it is same")
+# else:
+#     print("No, it is not same")
+
+
+# 2-Count the Number of Times a Digit Appears in All Numbers From 1 to n - Done - but it is not a profesional coding 
+# Num = 989
+# count = 0
+# Number = 9
+# while Num > 0:
+#     Num1 = Num%10
+#     print(Num1)
+#     if Num1 == Number:
+#         print("The number of times the digits appeared is:", count)
+#     else:
+#         print("No")
+#     count += 1
+#     Num = Num//10
+    
+
+
+# 3-Check if the Digits of a Number Are in Increasing Order - not able to solve
+# Num = 123867
+# Previous = 10
+# result = True
+# while Num>0:
+#     current = Num%10
+#     print(current)
+#     if Previous < current:
+#         result = False
+#         break
+#     Previous = current
+#     Num = Num//10
+
+
+
+
+# 4-Reverse the Digits of a Number Only If It's a Palindrome - Done but need to ask about if becuase it is showing wrong result
+# Num = int(input("Enter number:"))
+# Rev = 0
+# while Num>0:
+#     Rev = (Rev*10) + Num%10
+#     Num = Num//10
+#     print(Rev)
+#     if Num == Rev:
+#         print(Rev)
+#     else:
+#         print("No")
+        
+    
+
+# 5-Find the Smallest Number With a Given Digit Sum
+
+
+
+
+
+
+
+
+# 6-Count How Many Times You Need to Multiply Digits Until You Reach a Single Digit 
+# (Multiplicative Persistence) - Need to check 
+# Num = int(input("Enter number:"))
+# count = 0
+# while Num != 0:
+#     D = 1
+#     Rem = Num%10
+#     D = Rem*D
+#     count += 1
+#     print(D)
+#     Num = Num//10
+#     Num = Num*D
+#     if D<10:
+#         print("Single", D)
+
+# Num = 89
+# i = 1
+# count = 0
+# while Num != 0:
+#     count += 1
+#     Rem = Num%10
+#     i = Rem*Num
+#     Num = Num//10
+#     i += 1
+#     if i<10:
+#         print(count)
+#         print(i)
+#         break 
+#     Num = i
+
+
+# 7-Find the Largest Digit in a Number - need to ask
+# Num = int(63963)
+# i = 1
+# while Num>0:
+#     Rem = Num%10
+#     print(Rem)
+#     Num = Num//10
+#     if Rem >= 9:
+#         print("The largest number is:", Rem)
+#     elif Rem > 8:
+#         print("The largest number is:", Rem)
+#     elif Rem >= 7:
+#         print("The largest number is:", Rem)
+#     elif Rem > 6:
+#         print("The largest number is:", Rem)
+#     elif Rem == 5:
+#         print("The largest number is:", Rem)
+#     elif Rem == 4:
+#         print("The largest number is:", Rem)
+#     elif Rem == 3:
+#         print("The largest number is:", Rem)
+#     elif Rem == 2:
+#         print("The largest number is:", Rem)
+#     else:
+#         print("The largest number is:", Rem)
+
+
+# 8-Print All Numbers Between 1 and 1000 Whose Sum of Digits Is a Prime
+
+
+# 9-Create a Program to Detect a "Mountain" in a List
+
+# 10-Print All Pairs (i, j) From 1 to 100 Where i * j Ends with 9
+
+
+# 11-Count How Many Prime Numbers Are Palindromes Between 1 and 1000
+
+
+
+
+# 12-Generate the First 10 Numbers Where the Number Equals the Sum of the Factorials of Its Digits - Check
+# for i in range(1001):
+#     Num = i
+#     print(i)
+#     Fact = 1
+#     J = 1
+#     while J<=Num:
+#         Fact *= J
+        
+#         J += 1
+#         print("Total sum:", Fact)
+
+
+# 13-Given a Number, Rearrange Its Digits to Form the Largest Possible Number - did get ans need to increase
+# Num = 45963
+# count = 1
+# while Num != 0:
+#     Num1 = Num%10
+#     print(Num1)
+#     Num = Num//10
+#     print("The number of times the digits appeared in number:", count)
+#     count += 1
+#     print()
+
+
+# 14-Print All Numbers from 1 to 100 That Contain No Repeated Digits - Need to check
+# for i in range(1, 101):
+#     Num = i
+#     print(i)
+#     while i>0:
+
+#------------------Pattern--------------------------------
+
+# * * * * * *
+# * * * * *
+# * * *
+# * *
+# *
+
+# r = 5
+# for i in range(r , 0, -1):
+#     for j in range(i):
+#         print("*", end = " ")
+#     print()
+
+
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+# * * * * * *
+# A = 7
+# for i in range(1 , A+1):
+#     for i in range(1 , i):
+#         print("*", end = " " ,)
+#     print()
+
+
+#           *
+#         * *
+#       * * *  
+#     * * * *
+#   * * * * * 
+# * * * * * *
+# T = 6
+# for i in range(1, T, 1):
+#     for j in range(i):
+#         print(j)
+#     print()
+
+
+# * * * * * *
+#   * * * * *
+#     * * * *
+#       * * *
+#         * *
+#           *
